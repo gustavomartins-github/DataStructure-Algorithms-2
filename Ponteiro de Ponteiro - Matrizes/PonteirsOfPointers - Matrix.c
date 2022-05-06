004b0458616217829a47ff76b11f8d01f2b72b2c
Gustavo Martins <72039007+gustavomartins-github@users.noreply.github.com>
2022-05-06 18:20:51 -0300

    fixing bug
    The function "createDinamicIntMatrix" was a critical bug because the return "matrix = &m" is not ok. The correct sintax was "*matrix = m". In another way, others incremations was coded in this version

--- Ponteiro de Ponteiro - Matrizes/PonteirsOfPointers - Matrix.c
+++ Ponteiro de Ponteiro - Matrizes/PonteirsOfPointers - Matrix.c
@@ -7,11 +7,21 @@ This tye of matrix is dynamic and more slow than the static one.
 */
 void createDinamicIntMatrix(int ***matrix, int rows, int columns) {
     int **m;
-    m = malloc(sizeof(int *) * rows); // number of rows and 8 = number of bytes per row
+    m = (int **)malloc(sizeof(int *) * rows); // number of rows and 8 = number of bytes per row
     for (int i = 0; i < rows; i++) {
-        m[i] = malloc(sizeof(int) * columns); // number of columns and 4 = number of bytes per column 
+        m[i] =(int*) malloc(sizeof(int) * columns); // number of columns and 4 = number of bytes per column 
     }
-    matrix = &m;
+    *matrix = m;
+}
+
+// this function prints the matrix
+void printMatrix(int **matrix, int rows, int columns) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 int main (void){
@@ -33,15 +43,28 @@ int main (void){
     This code bellow alocates memory for the matrix in the heap.
     This type of matrix is dynamic and more slow
     */
-    int **dinamicMatrix;
-    createDinamicIntMatrix(&dinamicMatrix,2,2);
-    dinamicMatrix[0][0] = 10;
-    dinamicMatrix[0][1] = 20;
-    dinamicMatrix[1][0] = 30;
-    dinamicMatrix[1][1] = 40;
-    printf("%d\n", dinamicMatrix[0][0]);
-    printf("%d\n", dinamicMatrix[0][1]);
-    printf("%d\n", dinamicMatrix[1][0]);
-    printf("%d\n", dinamicMatrix[1][1]);
+    int **dynamicMatrix;
+    createDinamicIntMatrix(&dynamicMatrix,4,4);
+    dynamicMatrix[0][0] = 10;
+    dynamicMatrix[0][1] = 20;
+    dynamicMatrix[0][2] = 30;
+    dynamicMatrix[0][3] = 40;
+    dynamicMatrix[1][0] = 50;
+    dynamicMatrix[1][1] = 60;
+    dynamicMatrix[1][2] = 70;
+    dynamicMatrix[1][3] = 80;
+    dynamicMatrix[2][0] = 90;
+    dynamicMatrix[2][1] = 10;
+    dynamicMatrix[2][2] = 11;
+    dynamicMatrix[2][3] = 12;
+    dynamicMatrix[3][0] = 13;
+    dynamicMatrix[3][1] = 14;
+    dynamicMatrix[3][2] = 15;
+    dynamicMatrix[3][3] = 16;
+    printf("Dynamic Matrix:\n");
+    printMatrix(dynamicMatrix,4,4);
+
+    free(dynamicMatrix);
+    free(staticMatrix);
     return 0;
 }
